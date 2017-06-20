67838a66af18e8d2523e2f17df6e2ce26e7cc4a5
jinux <jinguol999@163.com>
2017-06-21 03:05:38 +0800

    feature: list the installed apk and click to open it's detail page

--- app/src/main/java/com/example/jinux/apkpackup/MainActivity.kt
+++ app/src/main/java/com/example/jinux/apkpackup/MainActivity.kt
@@ -1,22 +1,41 @@
 package com.example.jinux.apkpackup
 
+import android.content.Context
+import android.content.pm.PackageInfo
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
 import org.jetbrains.anko.*
-import org.jetbrains.anko.sdk25.coroutines.onClick
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        listView {
+        val apkListView = listView {
+            padding = dip(6)
 
+            val installApks =  packageManager.getInstalledPackages(0)
+
+            adapter = MListAdapter(this@MainActivity, installApks)
+
+            onItemClickListener = object : AdapterView.OnItemClickListener {
+                override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    val intent = Intent()
+                    intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                    intent.data = Uri.fromParts("package", installApks[position].packageName, null)
+                    startActivity(intent)
+                }
+            }
         }
     }
 
@@ -38,5 +57,37 @@ class MainActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+}
+
+class MListAdapter : BaseAdapter {
+
+    private var mContext: Context
+
+    private var mData: MutableList<PackageInfo>
+
+    constructor(context: Context, data: MutableList<PackageInfo>) {
+        mContext = context
+        mData = data
+    }
+
+    override fun getItem(position: Int): Any {
+        return mData[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return mData.size
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val apkName = mData[position]
+        val apkNameTextView = TextView(mContext)
+        apkNameTextView.text = apkName.packageName
+        apkNameTextView.padding = apkNameTextView.dip(20)
+        return apkNameTextView
     }
 }
